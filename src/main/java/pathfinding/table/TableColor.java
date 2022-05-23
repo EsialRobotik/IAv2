7367aef7a21043de40af4db7bbde6fa3aa84460a
GaG <francois.prugniel@telecomnancy.net>
2022-05-24 00:55:16 +0200

    colors

--- src/main/java/pathfinding/table/TableColor.java
+++ src/main/java/pathfinding/table/TableColor.java
@@ -4,8 +4,8 @@ package pathfinding.table;
  * Created by icule on 28/03/17.
  */
 public enum TableColor {
-    COLOR_0("Bleu"),
-    COLOR_3000("Jaune");
+    COLOR_0("Jaune"),
+    COLOR_3000("Violet");
 
     private String colorName;
 
